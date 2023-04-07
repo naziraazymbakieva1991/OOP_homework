ce69cd574d0f825458f54299134bc814de5ac357
OOP <nazira.azymbakieva1991@gmail.com>
2023-04-07 18:52:46 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Ucburchruc ucburchruc = new Ucburchruc();
+
+        ucburchruc.a = 7;
+        ucburchruc.b = 13;
+        ucburchruc.c = 9;
+        ucburchruc.area();
+
+
+
+
+    }
+
+}
---
+++ src/Ucburchruc.java
@@ -0,0 +1,20 @@
+public class Ucburchruc {
+
+    int a;int b;int c;
+
+
+
+    public void area(){
+    int p = (a+b+c);
+
+        System.out.println(p);
+
+        int s = (a*b*c)/2;
+        System.out.println(s);
+    }
+
+
+}
+
+
+
